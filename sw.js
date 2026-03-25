518e1d01d3564f553c47ea229db521571b94cba3
lucid1123-0329 <lucid1123.0329@gmail.com>
2026-03-26 04:48:13 +0900

    fix: serviceWorkerPath 제거 - OneSignal 기본 SW파일(OneSignalSDKWorker.js) 사용

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  */
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE_NAME = 'yr-v40';
+const CACHE_NAME = 'yr-v41';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
